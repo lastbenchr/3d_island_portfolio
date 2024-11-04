646a84d01dd52c41fffa3a723af3f4a1cd7cf0df
Sooraj Yadav <sooraj0032@gmail.com>
2024-11-05 01:46:15 +0530

    import issue fixed in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Suspense, useEffect, useRef, useState } from "react";
 import sakura from "../assets/sakura.mp3";
 import { HomeInfo, Loader } from "../components";
 import { soundoff, soundon } from "../assets/icons";
-import { Bird, Island, Plane, Sky } from "../models";
+import { Bird, Plane, Sky, Island } from "../Models";
 
 const Home = () => {
   const audioRef = useRef(new Audio(sakura));
